cba1a85ef5ba20f8be1538885cc0f8aefc37505b
Ozzland <ozzland_dracor@hotmail.com>
2016-09-28 21:26:57 -0600

    calc updtes

--- other/cory/js/nav.js
+++ other/cory/js/nav.js
@@ -1,15 +1,29 @@
 //UI elements
 var content = $('#content');
 
-function loadCalc() {
-	document.getElementById("content").innerHTML='<object type="text/html" data="calc.html" ></object>';
-}
-function topBar() {
-	$("#topBar").on("click",function(){
-   	$("#content").load("/calc.html");
-   	});
+//function loadCalc() {
+//	document.getElementById("content").innerHTML='<object type="text/html" data="calc.html" ></object>';
+//}
+
+//function topBar() {
+//	$("#topBar").on("click",function(){
+//   	$("#content").load("/calc.html");
+//   	});
+//}
+
+
+
+function home() {
+	document.getElementById("content").innerHTML = '';
+	$("#content").load("home.html");
 }
 
-function topBar2() {
+function calc() {
+	document.getElementById("content").innerHTML = '';
 	$("#content").load("calc.html");
+}
+
+function sCalc() {
+	document.getElementById("content").innerHTML = '';
+	$("#content").load("sCalc.html");
 }
