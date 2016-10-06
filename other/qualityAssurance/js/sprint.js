be0b41ecf5c0cd6fe8f3076af683cc179a0ed4a5
LanceEddleman <ozzland_dracor@hotmail.com>
2016-10-06 17:03:58 -0600

    working on sprint menus

--- other/qualityAssurance/js/sprint.js
+++ other/qualityAssurance/js/sprint.js
@@ -13,6 +13,9 @@
 	var num = 9999;
 	var rCount = 0;
 	var cDate = '';
+	var sprintInfo = [];
+	var sprintOrder = [];
+	var clicked = 'allIssues';
 
 // Current Date
 	function showDate(){
@@ -30,21 +33,34 @@
 // Sprint List
 	function getSprintsList(){
 		console.log('build Sprint list');
-		//var SL = $("#sprints"); // sprintlist
 		var SL = $(document.getElementById("sprintList"));
 		var slc = 2; 			// sprint list counter
 
 		domo.get('/data/v1/qasi?orderby=sprint ascending').then(function(qasi){
 			qasi.forEach(function(item) {
-				if (item.sprintlist === null) {	
-					// console.log(item.sprintlist);
+				if (item.sprintlist === null) {	// empty or null
 				}
 				else {
-					SL.append('<option value="' + slc + '">' + item.sprintlist + '</option>');
-					slc = slc + 1;
-					console.log(item.sprintlist);
+					var sprintDate = item.sprintlist;
+					sprintInfo.push({sprintDate});
 				}
 			});
+		// ========== Sort by NAME Descending ==========
+	 		var sort = true;
+			if (sort){
+	 			sprintInfo.sort(function(a, b){
+					var nameA=a.sprintDate.toLowerCase();
+		 		 	var nameB=b.sprintDate.toLowerCase();
+		 		 	if (nameA > nameB){return -1;}
+		 		 	if (nameA < nameB){return 1;}
+		 		 	return 0;
+		 		 });
+		 	}
+			for(c=0; c<sprintInfo.length; c++) {
+			SL.append('<option value="' + slc + '">' + sprintInfo[c].sprintDate + '</option>');
+			slc = slc + 1;
+			console.log(sprintInfo[c].sprintDate);
+			}
 		});
 		showSelection('all',num);
 	}
@@ -59,6 +75,7 @@
 		if (optionText == 'Select Release') {
 			console.log('run RESET');
 			runReset();
+			showSelection('all',num);
 		}
 		else {
 			showSelection('all',num);
@@ -71,6 +88,7 @@
 		document.getElementById("fire").innerHTML = '';
 		fireDiv.append('<div id="groupTitle" class="title">Select A Release To Review</div>');
 		fireDiv.append('<div id="fireText" class="tableRows">&nbsp;</div>');
+		showSelection('all',num);
 	}
 
 // Show Slection
@@ -82,24 +100,26 @@
 		document.getElementById("fireText").innerHTML = sectionTitle;
 
 		if (sprintNo == 0 || sprintNo == 1) {
-			console.log("sprint = 0 or 1");
+			// console.log("sprint selected: " + sprintNo);
 			if (status == 'all') {
-				console.log("sprint = 0 or 1 and status = all");
+				// console.log("sprint = 0 or 1 and status = all");
 				document.getElementById("groupTitle").innerHTML = 'Sprint: ' + sprint;
 				domo.get('/data/v1/qasi?orderby=sprint descending').then(function(qasi){
 					rCount = 0;
+					className = 'tableRows';
 					qasi.forEach(function(item) {
 						console.log(item);
 						if(rCount % 2 === 0) {
-							fireText.append('<div class="tableRows fRowE"><div class="wsquad">' + item.squad + '</div><div class="wjira">' + item.jira + '</div><div class="wsummary">' + item.summary + '</div><div class="wsprint">' + item.sprint + '&nbsp;</div></div>');
+							className = 'tableRows fRowE';
 							rCount = rCount+1;
 						}
 						else {
-							fireText.append('<div class="tableRows fRow"><div class="wsquad">' + item.squad + '</div><div class="wjira">' + item.jira + '</div><div class="wsummary">' + item.summary + '</div><div class="wsprint">' + item.sprint + '&nbsp;</div></div>');
+							className = 'tableRows fRow';
 							rCount = rCount+1;
 						}
+						fireText.append('<div class="' + className + '"><div class="wsquad">' + item.squad + '</div><div class="wjira">' + item.jira + '</div><div class="wsummary">' + item.summary + '</div><div class="wsprint">' + item.sprint + '&nbsp;</div></div>');
 				   });
-				console.log('Rows: ' + rCount);	
+				// console.log('Rows: ' + rCount);	
 				document.getElementById("rCount").innerHTML = '';
 				postRowCount.append('Rows: ' + rCount);
 				});
@@ -191,3 +211,26 @@
 
 
 
+// Active menu
+	function serverActive(clicked) {
+		$("#fireAll a").removeClass('active');
+		$("#fire5 a").removeClass('active');
+		$("#fire10 a").removeClass('active');
+		$("#allIssues a").removeClass('active');
+		if(clicked === 'allIssues') {
+			$("#allIssues a").addClass('active');
+			console.log('clicked: ' + clicked);
+		}
+		if(clicked === 'fireAll') {
+			$("#fireAll a").addClass('active');
+			console.log('clicked: ' + clicked);
+		}
+		if(clicked === 'fire5') {
+			$("#fire5 a").addClass('active');
+			console.log('clicked: ' + clicked);
+		}
+		if(clicked === 'fire10') {
+			$("#fire10 a").addClass('active');
+			console.log('clicked: ' + clicked);
+		}
+	}
