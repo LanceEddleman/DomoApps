200535752876905b2562bb068c6b7cde106665e5
LanceEddleman <ozzland_dracor@hotmail.com>
2016-12-20 17:49:21 -0700

    Adding director filter option

--- other/qualityAssurance/js/team.js
+++ other/qualityAssurance/js/team.js
@@ -6,6 +6,7 @@
 	var teamPageTitle = $('#teamTitle');
 	var squadInfo = $('#squadInfo');
 	var squadList = $('#squadList');
+	var dl = $('#DL');
 
 // Global variables
 	var uMiss = '';
@@ -14,8 +15,10 @@
 	var uNum = 0;
 	var squad = '';
 	var tsState = 0;
+	var filtered = 0;
 	var teamList = [];
 	var teamLeader = [];
+	var distinct = [];
 
 // Display team intro title splash
 	function teamTitleFade() {
@@ -26,11 +29,10 @@
 
 // Get all data, create arrays
 	function getTeamList(){
-		domo.get('/data/v1/teamList').then(function(qaTeam){
-			// console.log("qaTeams ", qaTeam);
-
+		domo.get("/data/v1/teamList").then(function(qaTeam){
 			qaTeam.sort(directorSort);
 			uNum = 0;
+			var temp_uNum = 0;
 
 			for(var t = 0; t < qaTeam.length; t++)
 			{
@@ -59,36 +61,21 @@
 					else {features = features;}
 				}
 
-				// if (team.value === 'Leadership') {
-				// 	console.log(uNum);
-				// 	var temp_uNum = uNum;
-				// 	console.log(temp_uNum);
-				// 	uNum = 99;
-				// 	console.log(uNum);
-				// 	teamList.push({team:team,qa1:qa1,qa2:qa2,qa3:qa3,i1:i1,i2:i2,director:director,features:features,uNum:uNum});
-				// 	uNum = temp_uNum + 1;
-				// 	console.log(uNum);
-
-				//  console.log(team + ' : ', teamList[t]);
-				// }
-				// else {
-				// 	teamList.push({team:team,qa1:qa1,qa2:qa2,qa3:qa3,i1:i1,i2:i2,director:director,features:features,uNum:uNum});
-				// 	uNum = uNum + 1;
-				// 	console.log(team + ' : ', teamList[t]);
-				// }
-
 				if (team !== 'Leadership') {
 					teamList.push({team:team,qa1:qa1,qa2:qa2,qa3:qa3,i1:i1,i2:i2,director:director,features:features,uNum:uNum});
 					uNum = uNum + 1;
 					console.log(team + ' : ', teamList[t]);
 				}
 				else {
-					var temp_uNum = uNum;
+					temp_uNum = uNum;
 					uNum = 99;
+					console.log(uNum);
 					teamList.push({team:team,qa1:qa1,qa2:qa2,qa3:qa3,i1:i1,i2:i2,director:director,features:features,uNum:uNum});
 					teamLeader.push({team:team,qa1:qa1,qa2:qa2,qa3:qa3,i1:i1,i2:i2,director:director,features:features,uNum:uNum});
 
 					uNum = temp_uNum;
+					teamList = teamList;
+					teamLeader = teamLeader;
 					console.log(teamLeader[0].team + ' : ' + teamLeader[0].qa1 + ' : ', teamList[t]);
 				}
 			}
@@ -107,7 +94,7 @@
 
 // list all teams
 	function displayAll() {
-
+		console.log(teamLeader);
 		var tlteam = teamLeader[0].team;
 		var tlqa1 = teamLeader[0].qa1;
 		var tlqa2 = teamLeader[0].qa2;
@@ -168,6 +155,7 @@
 			//			onclick="teamStats(' + team + ')">
 			squadList.append(squad);
 		}
+		getDirectorList();
 	}
 
 // Display team status section
@@ -202,7 +190,6 @@
 
 // Director sort
 	function directorSort(a,b) {
-	// function compare(a,b) {
 		  if (a.team < b.team)
 		    return -1;
 		  if (a.team > b.team)
@@ -210,13 +197,116 @@
 		  return 0;
 	}
 
+	function sortDirector(a,b) {
+		  if (a < b)
+		    return -1;
+		  if (a > b)
+		    return 1;
+		  return 0;
+	}
 
+// Director List
+// use teamList create my own from that list
+// call director list in display all function
+	function getDirectorList(){
+		var dlc = 0; // director list counter
 
+		for (var i = 0; i < teamList.length; i++) {if (distinct.indexOf(teamList[i].director) === -1){distinct.push(teamList[i].director);}}
 
+		distinct.sort(sortDirector);
 
+		for(x = 0; x < distinct.length; x++) {
+			if (distinct[x] !== null && distinct[x] !== "") {
+				var dFilter = distinct[x];
+				dPop = $('<li id="link"><option value="' + dlc + '">' + distinct[x] + '</option></li>');
 
+				(function(){
+					console.log('director num: ' + dlc + ' - director value: ' + distinct[x]);		
+					var locallink = distinct[x];
+					dPop.click(function(){displayFilter(locallink);});				
+				})();
 
+				filter4 = distinct[x];
+				dlc = dlc + 1;
 
+				dl.append(dPop);
+			}
+		}
+	}
+
+// Display selected link
+	function displayFilter(link) {
+		console.log('Clicked Link: ' + link);
+
+		filtered = link;
+		console.log('running dFilter: ' + filtered);
+		//document.getElementById('squadList').innerHTML = '';
+		document.getElementById('DL').innerHTML = '';
+		displayFiltered(filtered);
+	}
+
+// Display filted list
+	function displayFiltered(dfLink) {
+		document.getElementById('squadList').innerHTML = '';
+		document.getElementById('DL').innerHTML = '';
+
+		for(var w = 0; w < teamList.length; w++) {
+			var team = teamList[w].team;
+			var qa1 = teamList[w].qa1;
+			var qa2 = teamList[w].qa2;
+			var qa3 = teamList[w].qa3;
+			var i1 = teamList[w].i1;
+			var i2 = teamList[w].i2;
+			var director = teamList[w].director;
+			var features = teamList[w].features;
+			var row = teamList[w].uNum;
+			var u = undefined;
+			var n = null;
+
+			console.log(row);
+
+			// // qa1
+			// if (qa1 === u || qa1 === n || qa1 === "") { qa1 = uMiss; } else { qa1 = qa1; }
+			// // qa2
+			// if (qa2 === u || qa2 === n || qa2 === "") { qa2 = uMiss; } else { qa2 = qa2; }
+			// // qa3
+			// if (qa3 === u || qa3 === n || qa3 === "") { qa3 = uMiss; } else { qa3 = qa3; }
+			// // intern1
+			// if (i1 === u || i1 === n || i1 === "" || i1 === 'TBH') { i1 = uMiss; } else { i1 = i1; }
+			// // intern2
+			// if (i2 === u || i2 === n || i2 === "" || i2 === 'TBH') { i2 = uMiss; } else { i2 = i2; }
+			// // director
+			// if (director === u || director === n || director === "" || director === 'TBH') { director = uMiss; } else { director = director; }
+			// // features
+			// if (features === u || features === n || features === "") { features = fMiss; } else {features = features;}
+		
+			var addRow = '<br>';
+			var qa = '';
+			if(qa1 === '') {} else{qa = qa1;}
+			if(qa2 === '') {} else{if(qa !== ''){qa = qa + addRow + qa2;}else{qa = qa2;}}
+			if(qa3 === '') {} else{if(qa !== ''){qa = qa + addRow + qa3;}else{qa = qa3;}}
+			if(i1 === '') {} else{if(qa !== ''){qa = qa + addRow + i1;}else{qa = i1;}}
+			if(i2 === '') {} else{if(qa !== ''){qa = qa + addRow + i2;}else{qa = i2;}}
+			
+			var classTeam = 'fullWidth floatLeft';
+
+			if (row === 99) {classTeam = classTeam + ' teamLeader';}			
+			else if (row % 2 === 0) {classTeam = classTeam + ' aR1 teamLight';}
+			else{classTeam = classTeam + ' aR2 teamDark';}
+			
+			if (features === fMiss) {classTeam = classTeam + ' md';}
+			
+			if (director.indexOf(dfLink) !== -1) {
+				squad = $('<div id="' + team + '" class="' + classTeam + '"><span class="floatLeft teamsList")>' + team + '</span><span class="floatLeft qaList">' + qa + '</span><span class="floatLeft featuresList">' + features + '</span><span class="floatLeft directorList">' + director + '</span></div>');
+				squadList.append(squad);
+			}
+			else if (dfLink === 'All') {
+				squad = $('<div id="' + team + '" class="' + classTeam + '"><span class="floatLeft teamsList")>' + team + '</span><span class="floatLeft qaList">' + qa + '</span><span class="floatLeft featuresList">' + features + '</span><span class="floatLeft directorList">' + director + '</span></div>');
+				squadList.append(squad);
+			}
+		}
+		getDirectorList();
+	}
 
 
 
