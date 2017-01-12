1317d9a51beefd901063bd1bdf7d2b591ad0c1ea
LanceEddleman <ozzland_dracor@hotmail.com>
2017-01-12 11:36:57 -0700

    finished dice game

--- other/cory/js/dGame.js
+++ other/cory/js/dGame.js
@@ -1,9 +1,14 @@
 
-var dice1 = $("#d1");
-var dice2 = $("#d2");
-var dice3 = $("#d3");
-var dice4 = $("#d4");
+var dice1 = $("#die1");
+var dice2 = $("#die2");
+var dice3 = $("#die3");
+var dice4 = $("#die4");
 var diceTotal = $("#d5");
+var dNum1 = 0;
+var dNum2 = 0;
+var dNum3 = 0;
+var dNum4 = 0;
+var tValue = 0;
 
 var sRow = $("#startRow");
 var cRound = $("#currRound");
@@ -35,40 +40,41 @@ function startGame() {
 	document.getElementById("nextRound").value = '-Next Round-';
 	document.getElementById("startRow").innerHTML = 'Round: ' + roundStart;
 	document.getElementById("rLeft").innerHTML = reRollCount;
+	document.getElementById("tScore").innerHTML = 0;
 }
 
 function rollRound(actualRound) {
-	console.log('roundCount: ' + roundCount);
+	// console.log(': started rollRound :');
+	// console.log('roundCount: ' + roundCount);
 
 	gRound = actualRound;
 	reRollCount = 5;
+	// number of reRolls remaining
 	document.getElementById("rLeft").innerHTML = reRollCount;
-		document.getElementById("d1").disabled = false;
-		document.getElementById("d2").disabled = false;
-		document.getElementById("d3").disabled = false;
-		document.getElementById("d4").disabled = false;
+	// enable all reroll buttons
+		document.getElementById("die1").disabled = false;
+		document.getElementById("die2").disabled = false;
+		document.getElementById("die3").disabled = false;
+		document.getElementById("die4").disabled = false;
 
+	// generate, store and post random numbers
 	var dv1 = randomIntFromInterval(min,max);
 	var dv2 = randomIntFromInterval(min,max);
 	var dv3 = randomIntFromInterval(min,max);
 	var dv4 = randomIntFromInterval(min,max);
+	dNum1 = dv1;
+	dNum2 = dv2;
+	dNum3 = dv3;
+	dNum4 = dv4;
 
-	// rTotal = (dv1 + dv2 + dv3 + dv4);
-
-	// if(dv1 === dv2 && dv1 === dv3 && dv1 === dv4) {
-	// 	rTotal = ((dv1 + dv2 + dv3 + dv4)*5);
-	// 	document.getElementById("d5").value = ' BONUS ' + rTotal;
-	// }
-	// else {
-	// 	rTotal = (dv1 + dv2 + dv3 + dv4);
-	// }
-
+	// call checkrolls
+	console.log('call checkRoll passing the random numbers');
 	checkRoll(dv1,dv2,dv3,dv4);
 
-	document.getElementById("d1").value = dv1;
-	document.getElementById("d2").value = dv2;
-	document.getElementById("d3").value = dv3;
-	document.getElementById("d4").value = dv4;
+	document.getElementById("die1").value = dv1;
+	document.getElementById("die2").value = dv2;
+	document.getElementById("die3").value = dv3;
+	document.getElementById("die4").value = dv4;
 	document.getElementById("nextRound").disabled = false;
 
 	if (gRound < 10) {
@@ -77,10 +83,10 @@ function rollRound(actualRound) {
 		document.getElementById("rScore").innerHTML = rTotal;
 	}
 	else {
-		document.getElementById("d1").disabled = true;
-		document.getElementById("d2").disabled = true;
-		document.getElementById("d3").disabled = true;
-		document.getElementById("d4").disabled = true;
+		document.getElementById("die1").disabled = true;
+		document.getElementById("die2").disabled = true;
+		document.getElementById("die3").disabled = true;
+		document.getElementById("die4").disabled = true;
 		document.getElementById("nextRound").disabled = true;
 		document.getElementById("startRow").innerHTML = '<input type="button" id="gameStart" value="Start Game" class="startButton" onclick="startGame()" />';
 		actualRound = 0;
@@ -88,44 +94,72 @@ function rollRound(actualRound) {
 	}
 }
 
-function rollDice(dNum,value) {
+function checkRoll(dv1,dv2,dv3,dv4) {
+		// console.log('random numbers are still numbers at this point');
+		// console.log('numbers: ' + (dv1 + dv2 + dv3 + dv4));
+		if(dv1 === dv2 && dv1 === dv3 && dv1 === dv4) {
+		rTotal = 100;
+		document.getElementById("d5").value = ' BONUS ' + rTotal;
+	}
+	else {rTotal = (dv1 + dv2 + dv3 + dv4);}
+}
+
+
+function rollDice(dieID){
 	reRollCount = reRollCount - 1;
-	document.getElementById("rLeft").innerHTML = reRollCount;
-	console.log(dNum);
-
-	//test
-	var dx1 = document.getElementById("d1").value;
-	var dx2 = document.getElementById("d2").value;
-	var dx3 = document.getElementById("d3").value;
-	var dx4 = document.getElementById("d4").value;
-
-	var oldValue = value;
-	if (oldValue === 'X') {value = 0;}
 	var nValue = randomIntFromInterval(min,max);
-	// console.log('old number: ' + value + ' :newValue: ' + nValue);
-	document.getElementById(dNum).value = nValue;
-	
-	rTotal = (rTotal-value)+nValue;
+	diceID = '';
+	var diceID = dieID;
+	document.getElementById(diceID).value = nValue;
+
+	if (diceID === "die1") {
+		tValue = dNum1;
+		dNum1 = nValue;
+	}
+	else if (diceID === "die2") {
+		tValue = dNum2;
+		dNum2 = nValue;
+	}
+	else if(diceID === "die3") {
+		tValue = dNum3;
+		dNum3 = nValue;
+	}
+	else if(diceID === "die4") {
+		tValue = dNum4;
+		dNum4 = nValue;
+	}
+	document.getElementById("rLeft").innerHTML = reRollCount;
+	// console.log("dice clicked: " + diceID);
+	// console.log("old dice value: " + tValue + " add 1 " + (tValue + 1));
+	// console.log();
+
+	var oldValue = tValue;
+	// console.log('old number: ' + oldValue + ' :newNumber: ' + nValue);
+
+	// var nValue = randomIntFromInterval(min,max);
+	rTotal = (rTotal-tValue)+nValue;
 	document.getElementById("d5").value = ' = ' + rTotal;
 	document.getElementById("rScore").innerHTML = rTotal;
 	
-	console.log(dx1 + dx2 + dx3 + dx4);
-	if(nValue === dx1 && nValue === dx2 && nValue === dx3 && nValue === dx4) {
-		rTotal = ((dv1 + dv2 + dv3 + dv4)*5);
-		document.getElementById("d5").value = ' BONUS ' + rTotal;
+	//console.log(dx1 + dx2 + dx3 + dx4);
+	if(nValue === dNum1 && nValue === dNum2 && nValue === dNum3 && nValue === dNum4) {
+		rTotal = ((dNum1 * 4)*5);
+		document.getElementById("d5").value = 'BONUS'; // + rTotal;
+		console.log('BONUS');
 		document.getElementById("rScore").innerHTML = rTotal;
 		reRollCount = 0;
+		document.getElementById("rLeft").innerHTML = reRollCount;
 	}
 
 	if(reRollCount === 0) {
-		document.getElementById("d1").disabled = true;
-		document.getElementById("d2").disabled = true;
-		document.getElementById("d3").disabled = true;
-		document.getElementById("d4").disabled = true;
+		document.getElementById("die1").disabled = true;
+		document.getElementById("die2").disabled = true;
+		document.getElementById("die3").disabled = true;
+		document.getElementById("die4").disabled = true;
 	}
 	else {
-		document.getElementById(dNum).disabled = true;
-		setTimeout(function(){document.getElementById(dNum).disabled = false;},1000);
+		document.getElementById(diceID).disabled = true;
+		setTimeout(function(){document.getElementById(diceID).disabled = false;},1000);
 	}
 }
 
@@ -135,17 +169,6 @@ function postRound() {
 	roundCount = roundCount + 1;
 	document.getElementById("startRow").innerHTML = 'Round: ' + roundCount;
 	rollRound(roundCount);
-}
-
-function checkRoll(dv1,dv2,dv3,dv4) {
-		if(dv1 === dv2 && dv1 === dv3 && dv1 === dv4) {
-		rTotal = 100;
-		document.getElementById("d5").value = ' BONUS ' + rTotal;
-	}
-	else {
-		rTotal = (dv1 + dv2 + dv3 + dv4);
-	}
-
 }
 
 function postGame() {
@@ -163,7 +186,7 @@ function how2() {
 
 function setValue(x) {
 	var nTest = document.getElementById("a").value;
-	console.log("current value: " + nTest);
+	// console.log("current value: " + nTest);
 	nSum = nSum + nStore;
 	if (nTest == 0) {
 		nStore = x;
@@ -174,10 +197,10 @@ function setValue(x) {
 		nStore = nStore + x;
 		document.getElementById("a").value = '';
 		document.getElementById("a").value = nStore;
-		console.log('nStore: ' + nStore);
+		// console.log('nStore: ' + nStore);
 	}
 	nSum = nStore;
-	console.log('Selected: ' + x);
+	// console.log('Selected: ' + x);
 }
 
 function postSum() {
@@ -185,8 +208,8 @@ function postSum() {
 	nTotal = Number(nSum);
 	// document.getElementById("a").value;
 	document.getElementById("a").value = nBase;
-	console.log('rTotal is: ' + (rTotal+1));
-	console.log('nTotal is: ' + nTotal);
+	// console.log('rTotal is: ' + (rTotal+1));
+	// console.log('nTotal is: ' + nTotal);
 	var f = rTotal + nTotal;
 		document.getElementById("b").value = '';
 		document.getElementById("b").value = f;
@@ -210,12 +233,19 @@ function clearCurrent(opt) {
 
 function randomIntFromInterval(min,max)
 {
-    // var x = Math.floor(Math.random()*(max-min+1)+min);
-    // console.log(x);
 	return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-
+// Value test
+function printWithType(val) {
+        //document.write('<pre>');
+        // document.write(val);
+        console.log(val);
+        // document.write(' ');
+        //document.writeln(typeof val);
+        console.log(typeof(val));
+        // document.write('</pre>');
+    }
 
 
 
