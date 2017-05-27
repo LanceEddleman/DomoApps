f383490f0cd64f3f9850ae8bb6322b8ac56ea747
Ozzland <ozzland_dracor@hotmail.com>
2017-05-26 20:43:53 -0600

    old data

--- other/runDomoDelete /scripts/domoLogin.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-echo "Welcome to CLI clean up tool!"
-echo "Please login into the server you would like to clean up"
-domo login
-#cd ..
-java -jar IdeaProjects/runDomoDelete/out/artifacts/runDomoDelete_jar/runDomoDelete.jar
--- other/runDomoDelete /scripts/getDesigns.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-domo ls > myDesignIds.txt
